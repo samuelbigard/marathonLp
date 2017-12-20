660907e5800795eab94696f207d009f877fe3df7
samuel.bigard <samuelbigard@gmail.com>
2017-12-20 20:33:56 +0100

    ajout menu

--- public/build/dist/js/main.js
+++ public/build/dist/js/main.js
@@ -1,1 +1,1 @@
-var menu=!1;$(".burger").on("click",function(){menu?($("nav").removeClass("active"),menu=!1):($("nav").addClass("active"),menu=!0)});
+var menu=!1;$(".burger").on("click",function(){menu?($(".burger .this").removeClass("active"),$("nav").removeClass("active"),menu=!1):($("nav").addClass("active"),$(".burger .this").addClass("active"),menu=!0)});
