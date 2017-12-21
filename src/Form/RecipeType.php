67d1d7219b57d7335fc55f1ab65a49b27ac272c5
hadrienchatelet <Tookotouk@gmail.com>
2017-12-21 03:22:05 +0100

    fix de l'edit

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -6,6 +6,7 @@ use App\Entity\Recipe;
 use App\Form\MediaType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
@@ -35,7 +36,7 @@ class RecipeType extends AbstractType
             ->add('preparationTime', TimeType::class)
             ->add('cookingTime', TimeType::class)
             ->add('astuce')
-            ->add('media', MediaType::class, array('compound'=>true))
+            ->add('media', FileType::class, array('mapped'=>false))
             ->add('submit', SubmitType::class,['label' => 'Ajouter la recette'])
         ;
     }
